d68753e82b06f62295df7548e2abf66ecb0d26ed
6ftClaud <klaudijuskung@gmail.com>
2021-06-02 17:09:55 +0300

    Increased speed, now also checks for aspect ratio

--- scrape.sh
+++ scrape.sh
@@ -2,7 +2,8 @@
 # Requires sed, idenfify(imagemagick), jq, curl, fdupes, bc
 
 directory="$HOME/Pictures/Backgrounds" # Directory to download all your images to
-desiredpixelcount=4000000 # Pixel count used to determine image quality. 5000000 is 5MP.
+desired_pixel_count=4000000 # Pixel count used to determine image quality. 4 Megapixels is good for 1440p.
+desired_aspect_ratio=1.7778 # <- this is 16:9 aspect ratio
 current_directory=$(pwd)
 cd $directory
 
@@ -48,15 +49,15 @@ for file in $(find . -type f -not -name "*.sh" -not -name "*.txt");do
     aspect_ratio=$(echo "$w/$h" | bc -l)
     
     # Desired aspect ratio (16:9 is ~1.7778)
-    if [[ $(echo "$aspect_ratio>1.7" | bc) -eq 1 && $(echo "$aspect_ratio<1.8" | bc) -eq 1 ]];then
+    if [[ $(echo "$aspect_ratio>($desired_aspect_ratio-0.5)" | bc) -eq 1 && $(echo "$aspect_ratio<($desired_aspect_ratio+0.5)" | bc) -eq 1 ]];then
         :
     else
         echo "$file aspect ratio is  incorrect: $aspect_ratio"
         rm $file;exit
     fi
     # Desired pixel amount
-    if [[ $pixelcount -lt $desiredpixelcount ]];then
-        echo "$file pixel count is less than $desiredpixelcount : $pixelcount"
+    if [[ $pixelcount -lt $desired_pixel_count ]];then
+        echo "$file pixel count is less than $desired_pixel_count : $pixelcount"
         rm $file
     fi
     )
